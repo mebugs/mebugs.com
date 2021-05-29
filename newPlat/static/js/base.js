4794c7273802f7b1b040cb6089294fad4c79ac08
米虫 <iam@qiantaoyuan.cn>
2021-05-29 20:13:29 +0800

    新增一键切换 Light Dark 风格

---
+++ newPlat/static/js/base.js
@@ -0,0 +1,23 @@
+var loc = window.localStorage;
+var show = loc.getItem("show") ? loc.getItem("show") : "light";
+document.body.classList.add(show);
+window.addEventListener("scroll", function () {
+	let top = document.body.scrollTop || document.documentElement.scrollTop;
+	if(top > 80) {
+		document.body.classList.add("headerbg");
+	} else {
+		document.body.classList.remove("headerbg");
+	}
+})
+// set show light or dark
+function setShow() {
+	if(show == "light") {
+		show = "dark";
+		document.body.classList.remove("light");
+	} else {
+		show = "light";
+		document.body.classList.remove("dark");
+	}
+	loc.setItem("show",show);
+	document.body.classList.add(show);
+}
