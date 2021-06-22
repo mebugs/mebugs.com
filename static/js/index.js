6544dc906f0cc1877c08decaec471ae34c26032e
mebugs <iam@qiantaoyuan.cn>
2021-06-22 09:04:49 +0800

    最新发布Swiper

--- static/js/index.js
+++ static/js/index.js
@@ -1,17 +1,26 @@
 var swiper;
+var fumer;
 window.addEventListener("DOMContentLoaded", function () {
 	// Page Init (ext IE)
 	// banner
 	swiper = new Swiper('.bann', {
+	    loop:true,
+		autoplay: {
+		    disableOnInteraction: false,
+		    stopOnLastSlide: false,
+		    delay: 3000
+		},
+		speed: 600,
+	    pagination: {el: '.swiper-pagination',clickable:true}
+	});
+	fumer = new Swiper('.fm', {
+		loop:true,
 	    autoplay: {
 	        disableOnInteraction: false,
 	        stopOnLastSlide: false,
-	        delay: 3000
+	        delay: 5000
 	    },
-		watchSlidesProgress: true,
-	    loop:true,
-		speed: 500,
-		grabCursor: true,
-	    pagination: {el: '.swiper-pagination'}
+		speed: 900,
+		navigation: {nextEl: '.swiper-button-next',prevEl:'.swiper-button-prev'}
 	});
 });
