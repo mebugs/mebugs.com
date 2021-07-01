bf829f9406d9c1d433bd3a85dd40f43ec0052406
mebugs <iam@qiantaoyuan.cn>
2021-07-01 08:16:43 +0800

    默认DARK样式

--- static/js/base.js
+++ static/js/base.js
@@ -1,5 +1,5 @@
 var loc = window.localStorage;
-var show = loc.getItem("show") ? loc.getItem("show") : "light";
+var show = loc.getItem("show") ? loc.getItem("show") : "dark";
 var backTimer;
 var bodyNode = document.body;
 bodyNode.classList.add(show);
