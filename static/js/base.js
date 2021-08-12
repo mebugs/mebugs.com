3c3f5301491afeb45434916aebb52e56d1a24e74
mebugs <iam@qiantaoyuan.cn>
2021-08-12 09:03:59 +0800

    POST MENU

--- static/js/base.js
+++ static/js/base.js
@@ -10,6 +10,9 @@ var sTop = bodyNode.scrollTop || document.documentElement.scrollTop;
 var mHeight = false;
 var mRHeight = false;
 var mEnd = false;
+var mbm = false;
+var mbpm = false;
+var pomenu =  document.getElementById("pomenu");
 if(cW > 1200) {
   mHeight = document.getElementById("menue") ? document.getElementById("menue").getBoundingClientRect().top - 90 + sTop : false;
   mRHeight = mHeight ? document.getElementById("rhot").getBoundingClientRect().top + sTop : false;
@@ -41,6 +44,10 @@ window.addEventListener("scroll", function () {
       }
     }
 	}
+  // 滑动关闭目录
+  if(cW < 1200 && pomenu) {
+    doPmenu(false);
+  }
 })
 // update 20 since five sc
 var upF = 0;
@@ -73,13 +80,36 @@ function setShow() {
 	loc.setItem("show",show);
 	bodyNode.classList.add(show);
 }
-var mbm = false;
 function setMbm() {
   mbm = !mbm;
   if(mbm) {
     bodyNode.classList.add("opmenu");
+    if(pomenu) {
+       doPmenu(false);
+    }
   }else{
     bodyNode.classList.remove("opmenu");
+  }
+}
+
+function doPmenu(down) {
+  if(down) {
+    mbpm = !mbpm;
+    if(mbpm) {
+      bodyNode.classList.add("oppmenu");
+      pomenu.innerText="关闭目录";
+      if(mbm) {
+        mbm = !mbm;
+        bodyNode.classList.remove("opmenu");
+      }
+    }else{
+      bodyNode.classList.remove("oppmenu");
+      pomenu.innerText="打开目录";
+    }
+  }else{
+    mbpm = false;
+    bodyNode.classList.remove("oppmenu");
+    pomenu.innerText="打开目录";
   }
 }
 // back top 
