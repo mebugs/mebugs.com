5817b61b86f544da362b7f7f185617a8cd67b70b
mebugs <iam@qiantaoyuan.cn>
2022-07-20 07:54:52 +0800

    TOPD

--- front/static/js/base.js
+++ front/static/js/base.js
@@ -4,13 +4,14 @@ var loc = window.localStorage;
 var the = loc.getItem("the") ? loc.getItem("the") : "light";
 bodyNode.classList.add(the);
 setTimeout(function() {bodyNode.classList.add("trans");}, 100);
+ready(initIn)
 window.addEventListener("scroll", function () {
-	sTop = bodyNode.scrollTop || document.documentElement.scrollTop;
-	if(sTop > 280) {
-		bodyNode.classList.add("sh");
-	} else {
-		bodyNode.classList.remove("sh");
-	}
+	// sTop = bodyNode.scrollTop || document.documentElement.scrollTop;
+	// if(sTop > 280) {
+	// 	bodyNode.classList.add("sh");
+	// } else {
+	// 	bodyNode.classList.remove("sh");
+	// }
 });
 // 导航栏
 function doMenu() {
@@ -26,4 +27,23 @@ function theCheck(t) {
   loc.setItem("the",t);
   the = t;
   bodyNode.classList.add(the);
+}
+function initIn() {
+  // setTimeout(function() {bodyNode.classList.add("init");}, 200);
+}
+
+function ready(fn){
+  if(document.addEventListener){
+    document.addEventListener('DOMContentLoaded',function(){
+      document.removeEventListener('DOMContentLoaded',arguments.callee);
+      fn();
+    });
+  }else if(document.attachEvent){
+    document.attachEvent('onreadystatechange',function(){
+      if(document.readystate=='complete'){
+        document.dispatchEvent('onreadystatechange',arguments.callee);
+        fn();
+      }
+    })
+  }
 }
